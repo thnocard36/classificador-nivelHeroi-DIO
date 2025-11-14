9dbf6b083f2e235618f7210e9e85cb3cd7881fc3
thnocard36 <thnocard35@hotmail.com>
2025-11-14 00:28:27 -0300

    Estrurado os níveis de 'XP' do Heroi

--- src/teste.js
+++ src/teste.js
@@ -1,15 +1,44 @@
 // Declarando as variáveis "nome" e "nivelXP"
 let nomePersonagem = "CJ"
-let nivelXP = 10000
+let nivelXP = 500
 
-console.log("Olá, " +nome)
+console.log("Olá, " +nomePersonagem)
 console.log()
 
+/* switch(nivelXP) {
+    case 1000:
+        console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'FERRO'")
+        break
+
+    case 1001:
+    case 2000:
+        console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'BRONZE'")
+        break
+
+    case 2001:
+    case 5000:
+        console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'PRATA'")
+        break
+
+    default:
+        console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'RADIANTE'")
+} */
+
 // Algumas condições..
-if(nivelXP < 1000) {
+if(nivelXP < 1000) { // Se for menor que 1000... 
     console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'FERRO'")
-} else if(nivelXP === 1001 && 2000) {
+} else if(nivelXP >= 1001 && nivelXP <= 2000) { // Se for maior que 1001 e menor que 2000...
     console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'BRONZE'")
+} else if(nivelXP >= 2001 && nivelXP <= 5000) {
+    console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'PRATA'")
+} else if(nivelXP >= 6001 && nivelXP <= 7000) {
+    console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'OURO'")
+} else if(nivelXP >= 7001 && nivelXP <= 8000) {
+    console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'PLATINA'")
+} else if(nivelXP >= 8001 && nivelXP <= 9000) {
+    console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'ASCEDENTE'")
+} else if(nivelXP >= 9001 && nivelXP <= 10000) {
+    console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'IMORTAL'")
 } else {
     console.log("O Herói " + nomePersonagem + ", está no nivel " + nivelXP + " 'RADIANTE'")
 }
